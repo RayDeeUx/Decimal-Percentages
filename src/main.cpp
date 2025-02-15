b223b47be0b3cd4d7402dd77d9f78ae29464e226
Erymanthus | RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2025-02-15 09:24:50 -0500

    ??????????????????????????????????????????????????????????????????????????????????????????????????????????????

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@
 #define PREFERRED_HOOK_PRIO (-3999) // because for some incredible reason QOLMod changes a level's levelType value for a split second and now this hook prio's here to work around that
 #define DEATHSCREENTWEAKS "raydeeux.deathscreentweaks"
 
-static const std::regex percentageRegex(R"(^(?:(?:\d+(?:\.\d+)?%)([^\n\d]*))+(\d+?%)$)", std::regex::optimize | std::regex::icase);
+static const std::regex percentageRegex(R"(^(?:(?:\d+(?:\.\d+)?%)([^\n\d]*))+(\d+%)$)", std::regex::optimize | std::regex::icase);
 // see https://regex101.com/r/jlTQrI/2 for context, and https://regex101.com/r/poPUOK/1 for the better version
 static const std::regex trailingZeroesRegex(R"((.*[^0])(0+)$)", std::regex::optimize | std::regex::icase);
 // see https://regex101.com/r/j5IVLk/2 for context.
