535952995925b963ce6d7075e909f0feca07bbe8
Erymanthus | RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2025-02-15 09:23:50 -0500

    ????????????????????????????????//

--- src/main.cpp
+++ src/main.cpp
@@ -264,9 +264,9 @@ class $modify(MyPlayLayer, PlayLayer) {
 		});
 	}
 	void updateProgressbar() {
-		if (!getBool("enabled") || getBool("ignorePercentageLabel") || !m_level || m_level->isPlatformer() || !m_percentageLabel) return PlayLayer::updateProgressbar();
-		const std::string& percentLabelText = m_percentageLabel->getString();
 		PlayLayer::updateProgressbar();
+		if (!getBool("enabled") || getBool("ignorePercentageLabel") || !m_level || m_level->isPlatformer() || !m_percentageLabel) return;
+		const std::string& percentLabelText = m_percentageLabel->getString();
 		std::smatch match;
 		if (!std::regex_match(percentLabelText, match, percentageRegex)) return;
 		std::string newBestSeparator = match[1].str();
